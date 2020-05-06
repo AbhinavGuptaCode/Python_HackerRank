d160cccdb4c562e54092b76ee8ef0ca6a74fbae0
AbhinavGuptaCode <60182791+AbhinavGuptaCode@users.noreply.github.com>
2020-05-06 17:44:29 +0530

    Add files via upload

---
+++ PracticePythonStringsWhat's Your Name.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on (date)
+
+@author: %AbhinavGuptaCode
+"""
+
+
+
+def print_full_name(a, b):
+    print("Hello",a,b+"! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
